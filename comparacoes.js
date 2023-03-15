c1e1104c96b324e360663006c2380977740b8435
MATHEUS DE OLIVEIRA CAVALCANTE <99699238+M4TTHEW-OC@users.noreply.github.com>
2023-03-15 18:08:50 -0300

    Add files via upload

---
+++ comparacoes.js
@@ -0,0 +1,20 @@
+// == (compraração implícita )
+
+const numero = 5 
+const texto = "5";
+
+console.log(numero ==  texto)
+console.log(numero ===  texto)
+
+//typeof
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == só compara o valor
+// === compara o valor é o tipo de dado 
+
+//comversão explícita
+
+Number()
+String()
---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Ju"
+const idade = 2021-1981;
+const cidadeDeNacimento = "Brasília"
+
+//  const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de "
+//  + cidadeDeNacimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade ${cidadeDeNacimento}`;
+console.log(apresentacao);
